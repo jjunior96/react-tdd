1b01ab48fd62df7e6643425b7c81e9f54ba348d6
Junior Alves <jjalves96@gmail.com>
2020-10-29 08:41:06 -0300

    test: add new message into component

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -7,7 +7,7 @@ describe('<Main />', () => {
     const { container } = render(<Main />);
 
     expect(
-      screen.getByRole('heading', { name: /react TDD/i })
+      screen.getByRole('heading', { name: /react com TDD/i })
     ).toBeInTheDocument();
 
     // Snapshot
